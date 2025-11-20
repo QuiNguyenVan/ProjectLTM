10f4ddacf3ae5697419dcdbfaaa259c002ca17fa
Nguyen Thanh Lap <nguyenthanhlap205@gmail.com>
2025-11-20 14:05:17 +0700

    update dbConn before switching to Lap

--- src/main/java/Model/DAO/DBConnection.java
+++ src/main/java/Model/DAO/DBConnection.java
@@ -4,17 +4,17 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class DBConnection {
- private static final String URL = "jdbc:mysql://localhost:3306/PROJECTLTM";
- private static final String username="root";
- private static final String password="";
- 	public static Connection getConnection()
- 	{
- 		Connection conn=null;
- 		try
- 		{
- 			Class.forName("com.mysql.cj.jdbc.Driver");
- 			conn=DriverManager.getConnection(URL,username,password);
- 		}catch(Exception e) {e.getStackTrace();}
- 		return conn;
- 	}
+    private static final String URL = "jdbc:mysql://localhost:3306/PROJECTLTM";
+    private static final String USER = "root";
+    private static final String PASS = "";
+
+    public static Connection getConnection() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            return DriverManager.getConnection(URL, USER, PASS);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
 }
